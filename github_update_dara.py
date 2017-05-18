5f155a3b8ca3e8219b252e58430f39bd4ac2aa7b
ArmanHunanyan <arman.hunanyan@gmail.com>
2017-05-19 01:54:46 +0400

    Added wip clone/update
    Added wip clone/update

--- github_update_dara.py
+++ github_update_dara.py
@@ -4,6 +4,7 @@ import sys
 import requests, pprint, os, zipfile, shutil, glob, xml.etree.ElementTree, getpass
 import json
 from os.path import expanduser
+#from git import Repo
 
 home = expanduser("~")
 config_file_path = os.path.join(home, ".dara")
@@ -100,80 +101,184 @@ if not os.path.exists(DARA_RELEASES_PATH):
     os.makedirs(DARA_RELEASES_PATH)
 
 # download and extract
-for kit in KITS:
-    try:
-        rest_api_response = requests.get(BASE_URL % kit, auth=(USERNAME, PASSWORD))
-        rest_api_response.raise_for_status()
-    except requests.exceptions.HTTPError as err:
-        print err
-        sys.exit(1)
-    data = rest_api_response.json()
+def download_releases():
+    for kit in KITS:
+        try:
+            rest_api_response = requests.get(BASE_URL % kit, auth=(USERNAME, PASSWORD))
+            rest_api_response.raise_for_status()
+        except requests.exceptions.HTTPError as err:
+            print err
+            sys.exit(1)
+        data = rest_api_response.json()
 
-    target_path = os.path.join(DARA_KITS_PATH, kit)
-    target_cfg = os.path.join(target_path, "index.cfg")
-    if os.path.exists(target_cfg) and os.path.isfile(target_cfg):
-        repo_version = data['tag_name']
-        config_xml = xml.etree.ElementTree.parse(target_cfg).getroot()
-        local_version = config_xml.attrib["version"]
-        if local_version == repo_version:
-            print "up to date %s..." % data['zipball_url']
-            continue
+        target_path = os.path.join(DARA_KITS_PATH, kit)
+        target_cfg = os.path.join(target_path, "index.cfg")
+        if os.path.exists(target_cfg) and os.path.isfile(target_cfg):
+            repo_version = data['tag_name']
+            config_xml = xml.etree.ElementTree.parse(target_cfg).getroot()
+            local_version = config_xml.attrib["version"]
+            if local_version == repo_version:
+                print "up to date %s..." % data['zipball_url']
+                continue
 
-    if os.path.exists(target_path):
-        shutil.rmtree(target_path)
+        if os.path.exists(target_path):
+            shutil.rmtree(target_path)
 
-    print "downloading %s..." % data['zipball_url']
+        print "downloading %s..." % data['zipball_url']
 
-    zip_file_path = download_file(kit, data['zipball_url'])
-    extracted_folder_name = extract_zip_file(zip_file_path, DARA_KITS_PATH)
+        zip_file_path = download_file(kit, data['zipball_url'])
+        extracted_folder_name = extract_zip_file(zip_file_path, DARA_KITS_PATH)
 
-    extracted_folder_name = os.path.join(DARA_KITS_PATH, extracted_folder_name)
+        extracted_folder_name = os.path.join(DARA_KITS_PATH, extracted_folder_name)
 
-    # retrieve actual kit name (just in case it's not the same as the github repo name)
-    index_file = os.path.join(extracted_folder_name, "index.cfg")
+        # retrieve actual kit name (just in case it's not the same as the github repo name)
+        index_file = os.path.join(extracted_folder_name, "index.cfg")
+        index_xml = xml.etree.ElementTree.parse(index_file).getroot()
+        real_kit_name = index_xml.attrib["kit"]
+
+        os.rename(extracted_folder_name, target_path)
+        os.remove(zip_file_path)
+
+    # duplicate dara folder
+    temp_directory = os.path.join(DARA_RELEASES_PATH, "tmp")
+    shutil.copytree(DARA_PATH, temp_directory)
+
+    # # delete cruft
+    for directory,subdirs,files in os.walk(temp_directory):
+        if '.gitignore' in files:
+            os.unlink(os.path.join(temp_directory, directory, '.gitignore'))
+        if '.gitmodules' in files:
+            os.unlink(os.path.join(temp_directory, directory, '.gitmodules'))
+        if '.gitattributes' in files:
+            os.unlink(os.path.join(temp_directory, directory, '.gitattributes'))
+        if '.git' in subdirs:
+            shutil.rmtree(os.path.join(temp_directory, directory, '.git'))
+        for pyc_file in [f for f in files if f.lower().endswith('.pyc')]:
+            os.unlink(pyc_file)
+
+    # retrieve dara version
+    index_file = os.path.join(temp_directory, "index.cfg")
     index_xml = xml.etree.ElementTree.parse(index_file).getroot()
-    real_kit_name = index_xml.attrib["kit"]
+    dara_version = index_xml.attrib["version"]
 
-    os.rename(extracted_folder_name, target_path)
-    os.remove(zip_file_path)
+    release_dirname = os.path.join(DARA_RELEASES_PATH, KIT_NAME + "_" + str(dara_version))
 
-# duplicate dara folder
-temp_directory = os.path.join(DARA_RELEASES_PATH, "tmp")
-shutil.copytree(DARA_PATH, temp_directory)
+    if os.path.isdir(release_dirname):
+        shutil.rmtree(release_dirname)
 
-# # delete cruft
-for directory,subdirs,files in os.walk(temp_directory):
-    if '.gitignore' in files:
-        os.unlink(os.path.join(temp_directory, directory, '.gitignore'))
-    if '.gitmodules' in files:
-        os.unlink(os.path.join(temp_directory, directory, '.gitmodules'))
-    if '.gitattributes' in files:
-        os.unlink(os.path.join(temp_directory, directory, '.gitattributes'))
-    if '.git' in subdirs:
-        shutil.rmtree(os.path.join(temp_directory, directory, '.git'))
-    for pyc_file in [f for f in files if f.lower().endswith('.pyc')]:
-        os.unlink(pyc_file)
+    os.rename(temp_directory, release_dirname)
 
-# retrieve dara version
-index_file = os.path.join(temp_directory, "index.cfg")
-index_xml = xml.etree.ElementTree.parse(index_file).getroot()
-dara_version = index_xml.attrib["version"]
+    # zip release directory
+    release_zipname = release_dirname + ".zip"
 
-release_dirname = os.path.join(DARA_RELEASES_PATH, KIT_NAME + "_" + str(dara_version))
+    temp_file = os.path.join(DARA_RELEASES_PATH, "tmp")
+    shutil.make_archive(temp_file, 'zip', release_dirname)
 
-if os.path.isdir(release_dirname):
+    if os.path.isfile(release_zipname):
+        os.unlink(release_zipname)
+    os.rename(temp_file + ".zip", release_zipname)
+
     shutil.rmtree(release_dirname)
 
-os.rename(temp_directory, release_dirname)
+# update/clone wip
+def update_wip():
+    try:
+        os.mkdir(DARA_WIP_PATH)
+    except:
+        pass
 
-# zip release directory
-release_zipname = release_dirname + ".zip"
+    for kit in KITS:
+        try:
+            repo_url = 'https://github.com/adamohern/%s' % kit
+            dest_path = os.path.join(DARA_WIP_PATH, kit)
+            if os.path.exists(dest_path):
+                print 'Update', kit
+#                repo = Repo(dest_path)
+#                origin = repo.remotes.origin
+#                origin.pull(rebase=True)
+#                origin.push()
+                os.chdir(dest_path)
+                os.system("git pull --rebase")
+                os.system("git push")
+            else:
+                print "Cloning", kit
+                #Repo.clone_from(repo_url, os.path.join(DARA_WIP_PATH, kit))
+                os.system("git clone {0} {1}".format(repo_url, dest_path))
 
-temp_file = os.path.join(DARA_RELEASES_PATH, "tmp")
-shutil.make_archive(temp_file, 'zip', release_dirname)
+        except requests.exceptions.HTTPError as err:
+            print err
+            sys.exit(1)
+        continue
+        data = rest_api_response.json()
 
-if os.path.isfile(release_zipname):
-    os.unlink(release_zipname)
-os.rename(temp_file + ".zip", release_zipname)
+        target_path = os.path.join(DARA_KITS_PATH, kit)
+        target_cfg = os.path.join(target_path, "index.cfg")
+        if os.path.exists(target_cfg) and os.path.isfile(target_cfg):
+            repo_version = data['tag_name']
+            config_xml = xml.etree.ElementTree.parse(target_cfg).getroot()
+            local_version = config_xml.attrib["version"]
+            if local_version == repo_version:
+                print "up to date %s..." % data['zipball_url']
+                continue
 
-shutil.rmtree(release_dirname)
+        if os.path.exists(target_path):
+            shutil.rmtree(target_path)
+
+        print "downloading %s..." % data['zipball_url']
+
+        zip_file_path = download_file(kit, data['zipball_url'])
+        extracted_folder_name = extract_zip_file(zip_file_path, DARA_KITS_PATH)
+
+        extracted_folder_name = os.path.join(DARA_KITS_PATH, extracted_folder_name)
+
+        # retrieve actual kit name (just in case it's not the same as the github repo name)
+        index_file = os.path.join(extracted_folder_name, "index.cfg")
+        index_xml = xml.etree.ElementTree.parse(index_file).getroot()
+        real_kit_name = index_xml.attrib["kit"]
+
+        os.rename(extracted_folder_name, target_path)
+        os.remove(zip_file_path)
+
+    # duplicate dara folder
+    temp_directory = os.path.join(DARA_RELEASES_PATH, "tmp")
+    shutil.copytree(DARA_PATH, temp_directory)
+
+    # # delete cruft
+    for directory, subdirs, files in os.walk(temp_directory):
+        if '.gitignore' in files:
+            os.unlink(os.path.join(temp_directory, directory, '.gitignore'))
+        if '.gitmodules' in files:
+            os.unlink(os.path.join(temp_directory, directory, '.gitmodules'))
+        if '.gitattributes' in files:
+            os.unlink(os.path.join(temp_directory, directory, '.gitattributes'))
+        if '.git' in subdirs:
+            shutil.rmtree(os.path.join(temp_directory, directory, '.git'))
+        for pyc_file in [f for f in files if f.lower().endswith('.pyc')]:
+            os.unlink(pyc_file)
+
+    # retrieve dara version
+    index_file = os.path.join(temp_directory, "index.cfg")
+    index_xml = xml.etree.ElementTree.parse(index_file).getroot()
+    dara_version = index_xml.attrib["version"]
+
+    release_dirname = os.path.join(DARA_RELEASES_PATH, KIT_NAME + "_" + str(dara_version))
+
+    if os.path.isdir(release_dirname):
+        shutil.rmtree(release_dirname)
+
+    os.rename(temp_directory, release_dirname)
+
+    # zip release directory
+    release_zipname = release_dirname + ".zip"
+
+    temp_file = os.path.join(DARA_RELEASES_PATH, "tmp")
+    shutil.make_archive(temp_file, 'zip', release_dirname)
+
+    if os.path.isfile(release_zipname):
+        os.unlink(release_zipname)
+    os.rename(temp_file + ".zip", release_zipname)
+
+    shutil.rmtree(release_dirname)
+
+update_wip()
+
